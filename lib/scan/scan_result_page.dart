86e9362bc6741f29178c6b58eed251b0b15fc5f6
Biritwum-Nyarko Kwasi Manu <51910665+kwasimbnyarko@users.noreply.github.com>
2023-01-18 00:34:29 +0000

    adding scan result page

--- lib/scan/scan_result_page.dart
+++ lib/scan/scan_result_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+// TODO Implement this library.import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:was_here/models/mark_attendance_model.dart';
 import 'package:was_here/utils/app_routes.dart';
